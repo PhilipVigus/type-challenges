9a057e8c87b4e982b77feba29e68713a664d05e9
Phil Vigus <philvigus@gmail.com>
2023-07-03 17:04:44 +0200

    Complete exercises 43 and 189

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,9 @@
-type MyExclude<T, U> = any
+/**
+ * This makes use of the distributive nature of conditional types applied
+ * to generic types. For example:
+ * MyExclude<'a' | 'b' | 'c', 'a'> = 'b' | 'c' because
+ * 'a' | 'b' | 'c' extends 'a' becomes
+ * 'a' extends 'a' | 'b' extends 'a' | 'c' exetnds 'a'
+ * with positive results being filtered out using ? never : T
+ */
+type MyExclude<T, U> = T extends U ? never : T
--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,13 @@
-type MyAwaited<T> = any
+// allows for deeply nested promises like Promise<Promise<Promise<string | boolean>>>
+type MyAwaited<T extends PromiseLike<any | PromiseLike<any>>> =
+  // Does T extend PromiseLike<infer V>?
+  // infer V captures the type being wrapped in type variable V
+  T extends PromiseLike<infer V>
+    ? // Does V extend PromiseLike<any>?
+      V extends PromiseLike<any>
+      ? // If it does, call MyAwaited recursively on the wrapped type
+        MyAwaited<V>
+      : // If it doesn't, return the wrapped type
+        V
+    : // Handle attempts to use the type on variables that aren't 'promise-likes'
+      never
