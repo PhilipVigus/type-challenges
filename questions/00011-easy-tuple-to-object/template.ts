6c52f21ddc121d423234915dca79035874b4bfd5
Phil Vigus <philvigus@gmail.com>
2023-07-03 14:06:30 +0200

    Complete exercises 11, 14 and 18

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,8 @@
-type TupleToObject<T extends readonly any[]> = any
+/**
+ * T is a generic type
+ * extends readonly (string | number) means that it has to be a tuple containing strings or numbers or both
+ * [P in T[number]]: P iterates over each each individual element type in T[number] and assigns the same value to it
+ */
+type TupleToObject<T extends readonly (string | number)[]> = {
+  [P in T[number]]: P
+}
--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,7 @@
-type First<T extends any[]> = any
+/**
+ * T extends any[] - T is an array of any type
+ * T extends [] T is an empty array
+ *  if it is, return never
+ *  if it isn't, return the value of T[0]
+ */
+type First<T extends any[]> = T extends [] ? never : T[0]
