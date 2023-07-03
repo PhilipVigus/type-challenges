bc7698757c41b8afd9c2b1b8e927e4c63fce6d63
Phil Vigus <philvigus@gmail.com>
2023-07-03 11:09:38 +0200

    Complete easy pick exercise

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,14 @@
-type MyPick<T, K> = any
+/**
+ * T is a generic type
+ * keyof T gives a union of all the keys (property names) in T
+ * K extends keyof ensures that K contains only keys that are present in T
+ *
+ * [Key in K] is a mapped type declaration. Key is a placenholder that represents each key in the union type K
+ * T[Key] is an indexed access type. It gets the type of the property in type K with key name Key
+ *
+ * [Key in K]: T[Key] creates a new object using the keys from K, with their types set to the corresponding types
+ * of those keys in T
+ */
+type MyPick<T, K extends keyof T> = {
+  [Key in K]: T[Key]
+}
