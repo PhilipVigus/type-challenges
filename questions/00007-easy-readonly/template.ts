b0dc7213dcb0ec91ee5b49d4fb9089eddcbb270e
Phil Vigus <philvigus@gmail.com>
2023-07-03 11:44:23 +0200

    Complete easy readonly exercise

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,13 @@
-type MyReadonly<T> = any
+/**
+ * T is a generic type
+ * [P in key of T] iterates over each property P in T's keys
+ * T[P] gets the value of the property P from T
+ *
+ * [P in keyof T]: T[P] iterates over each property in T's keys
+ * and assigns each a value equal to the value of property P in T
+ *
+ * All that's left to do is make each property readyonly
+ */
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
