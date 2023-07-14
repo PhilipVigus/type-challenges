a8b8719f447db166ef486d1d055ff669dcf3f798
Phil Vigus <philvigus@gmail.com>
2023-07-14 16:12:49 +0200

    Complete easy push challenge

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,10 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal, Expect } from "@type-challenges/utils"
+
+type Includes<T extends readonly unknown[], U> = T extends [
+  infer First,
+  ...infer Rest
+]
+  ? Equal<First, U> extends true
+    ? true
+    : Includes<Rest, U>
+  : false
