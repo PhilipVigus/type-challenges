4df6f30102591cc58bbe8356bd512877848e17f8
Phil Vigus <philvigus@gmail.com>
2023-07-14 16:40:48 +0200

    Complete parameters challenge

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,13 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+/**
+ * T extends (...args: any[]) => any
+ * a generic constraint that ensures T is a function type that takes any number of arguments and returns some value
+ *
+ * The rest of the type is conditional
+ * T extends (...any: infer S) => any is the condition. infer S extracts the types of the arguments
+ * The conditional returns the types if T extends a function, otherwise it returns any
+ */
+type MyParameters<T extends (...args: any[]) => any> = T extends (
+  ...any: infer S
+) => any
+  ? S
+  : any
