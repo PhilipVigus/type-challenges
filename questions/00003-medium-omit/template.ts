72edbf310aaa3b15c45e3cd9429ff871dc4583d6
Phil Vigus <philvigus@gmail.com>
2023-07-21 15:13:20 +0200

    Complete return type and omit exercises

--- questions/00003-medium-omit/template.ts
+++ questions/00003-medium-omit/template.ts
@@ -1,1 +1,10 @@
-type MyOmit<T, K> = any
+/**
+ * This is a mapped type
+ * It maps the keys of T, iterating over each and assigning it to P
+ * It conditionally maps them
+ *      If P extends K then it omits them
+ *      If it doesn't then it maps to T[P]
+ */
+type MyOmit<T, K extends keyof T> = {
+  [P in keyof T as P extends K ? never : P]: T[P]
+}
